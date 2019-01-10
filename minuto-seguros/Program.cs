354d5a955044538a452a9a68d4b7dd1a57a435ad
Mariel Moura <mariel.moura@gmail.com>
2019-01-10 21:39:51 -0200

    Fixing Program.cs

--- minuto-seguros/Program.cs
+++ minuto-seguros/Program.cs
@@ -1,13 +1,8 @@
 ﻿using CodeHollow.FeedReader;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Xml;
-
 
 namespace minuto_seguros
 {
