c9ae64f7a0528c33f30d92e372b2bb82452268b7
Mariel Moura <mariel.moura@gmail.com>
2019-01-10 21:27:12 -0200

    first app commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Oportunidade de trabalho na Minuto Seguros
+===========================================
+
+Venha trabalhar na área de tecnologia da Minuto Corretora de Seguros. Utilizamos métodos ágeis para criação de software e nosso clima é de extrema colaboração. 
+
+Se você tem interesse em fazer parte de uma equipe multidisciplinar e que adora criar software com qualidade, siga os seguintes passos:
+
+Faça um fork desse projeto e faça um pull request com a resolução do seguinte problema:
+
+Exercício de programação: As principais palavras
+-------------------------------------------------------
+
+Você deverá criar um programa para obter automaticamente o conteúdo dos dez últimos tópicos publicados no blog da Minuto Seguros. Abaixo segue um link para auxiliá-lo nesse trabalho:
+
+http://www.minutoseguros.com.br/blog/feed/
+
+Caso tenha problemas em obter o feed, deixamos um arquivo feed.xml aqui no repositório.
+
+O seu programa deverá avaliar quais as dez principais palavras abordadas nesses tópicos e qual o número de vezes que elas aparecem. Também deverá exibir a quantidade de palavras por tópico. Além disso, deverão ser removidos os artigos e preposições nessa análise.
+
+A linguagem que mais utilizamos é C#. Porém, você poderá realizar o exercício na linguagem de programação de sua preferência.
+
+Como trabalhamos aqui
+----------
+
+Trabalhamos com uma boa infraestrutura, nosso hardware é muito bom e possuímos um ambiente de trabalho muito agradável. A empresa não possui hierarquias desnecessárias e você é convidado e desafiado a colaborar com todas as frentes de trabalho. Ou seja, aqui o “pitaco” é bem vindo!
+
+Nós acreditamos muito nos números para tomada de decisões. Nós possuímos diversas métricas e isso inclui também medir o nosso código. Acompanhamos esses indicadores para construir códigos sólidos e de fácil manutenção. Afinal, queremos a cada dia mais flexibilidade e continuar animados a evoluir nossas aplicações.
+
+Nosso trabalho é baseado em autogestão. Só existe uma regra de convivência na frente de TI: É proibido murmurar! Aqui as opiniões são discutidas, resolvidas e sempre chegamos a um consenso para melhorar a nossa convivência. Isso não foi descrito por um gerente de RH e sim por um desenvolvedor de software!
---
+++ minuto-seguros/Program.cs
@@ -0,0 +1,105 @@
+﻿using CodeHollow.FeedReader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace minuto_seguros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Feed feed = getFeed();
+                showWordCounterByFeed(feed);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+
+        }
+
+        static Feed getFeed()
+        {
+            string remoteFeed = "https://www.minutoseguros.com.br/blog/feed/";
+            string localFeed = "feed.xml";
+
+            try
+            {
+                return FeedReader.Read(remoteFeed);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    Console.WriteLine("Can't read rss remote feed: " + remoteFeed);
+                    Console.WriteLine("Trying to read local feed: " + localFeed);
+                    return FeedReader.ReadFromFile(localFeed);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    throw ex;
+                }
+            }
+
+        }
+
+        static void showWordCounterByFeed(Feed feed)
+        {
+            try
+            {
+                foreach (var item in feed.Items)
+                {
+                    var content = Regex.Replace(item.Content, "<.*?>", " ");
+
+                    IDictionary<string, int> wordsDictionary = new Dictionary<string, int>();
+
+                    var invalidWords = new[] { " ", "o", "a", "e", "para", "de", "do", "é", "se", "que", "com", "mais",
+                "por", "uma", "você", "em", "os", "não", "um", "no", "sem", "sua", "como", "ao", "está", "ser",
+                "seu", "já", "nem", "pelo", "fica", "pode", "quando", "nos", "dos", "há", "isso", "da", "pelas",
+                "outras", "porém", "lá", "vão", "tão", "as", "na", "essa", "pelos", "à", "das", "suas", "entre",
+                "até", "nas", "ou", "ele", "ela", "seja", "são", "além", "ter", "me"};
+
+                    foreach (string word in content.Split(' '))
+                    {
+                        string wordToLower = Regex.Replace(word.ToLower(), "[^a-záàâãéèêíïóôõöúçñ ]+", "", RegexOptions.Compiled);
+
+                        if (String.IsNullOrEmpty(wordToLower) || invalidWords.Contains(wordToLower))
+                            continue;
+
+                        if (wordsDictionary.ContainsKey(wordToLower))
+                            wordsDictionary[wordToLower] += 1;
+                        else
+                            wordsDictionary.Add(wordToLower, 1);
+                    };
+
+                    foreach (KeyValuePair<string, int> word in wordsDictionary.OrderByDescending(x => x.Value).Take(10))
+                    {
+                        Console.WriteLine(item.Title + " - " + word.Key + " - " + word.Value);
+                    }
+                
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
